8280a984dd455e80b421f89c8238c5632214673b
matheus-mp <matheusmachadopires777@gmail.com>
2026-04-13 16:23:18 -0300

    IsPalindrome

---
+++ IsPalindrome/IsPalindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace IsPalindrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome(-111));
+    }
+
+    public static bool IsPalindrome(int x)
+    {
+        bool pal;
+        string xs = Convert.ToString(x);
+        string r;
+        char[] arrXs = new char[xs.Length];
+        if (x < 0)
+        {
+            pal = false;
+        }
+        else
+        {
+            arrXs = xs.ToCharArray();
+
+            Array.Reverse(arrXs);
+
+            r = string.Join("", arrXs);
+            
+            if (r == xs)
+            {
+                pal = true;
+            }
+            else
+            {
+                pal = false;
+            }
+        }
+
+        return pal;
+    }
+}
