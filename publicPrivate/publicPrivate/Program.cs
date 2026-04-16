af073fd670eca154ed561d38a9cacd918df6b701
matheus-mp <matheusmachadopires777@gmail.com>
2026-04-16 17:19:47 -0300

    POO

---
+++ publicPrivate/publicPrivate/Program.cs
@@ -0,0 +1,57 @@
+﻿class Jogador
+{
+    private int energia;
+    private string nome;
+
+    public Jogador(string nome)
+    {
+        this.nome = nome;
+        energia = 100;
+    }
+
+    public int getEnergia()
+    {
+        return energia;
+    }
+
+    public void setEnergia(int e)
+    {
+        if (e < 0)
+        {
+            if (energia + e < 0 )
+            {
+                energia = 0;
+            }
+            else
+            {
+                energia += e;
+            }
+        }
+        else if (e > 0)
+        {
+            if (energia + e > 100)
+            {
+                energia = 100;
+            }
+            else
+            {
+                energia += e;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Jogador j1 = new Jogador("Jorge");
+        
+        j1.setEnergia(-30);
+        j1.setEnergia(-30);
+        j1.setEnergia(-30);
+        j1.setEnergia(-30);
+        
+        Console.WriteLine(j1.getEnergia());
+    }
+}
---
+++ this/this/Program.cs
@@ -0,0 +1,25 @@
+﻿class Calculos
+{
+    public int v1, v2;
+
+    public Calculos(int v1, int v2)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+    
+    public int Somar()
+    {
+        return v1 + v2;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculos calc1 = new Calculos(10, 2);
+        
+        Console.WriteLine(calc1.Somar());
+    }
+}
